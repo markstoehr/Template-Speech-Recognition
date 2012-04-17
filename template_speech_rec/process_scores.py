9ee18471b8c8faa1ce759ee9510906cb6ee2bb88
Mark Stoehr <stoehr.mark@gmail.com>
2012-04-16 19:25:25 -0500

    more sophisticated maxima finding

--- template_speech_rec/process_scores.py
+++ template_speech_rec/process_scores.py
@@ -5,4 +5,3 @@ def get_maxima(scores,radius):
     scores_idx = scores_idx[radius:-radius]
     return scores >= np.concatenate([np.inf*np.ones(radius),np.max(scores[scores_idx],axis=1),np.inf*np.ones(radius)])
     
-    
