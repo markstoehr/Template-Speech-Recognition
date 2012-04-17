f1fd9cb2bdcbe9b9508fe4f9e1e44515346d9737
Mark Stoehr <stoehr.mark@gmail.com>
2012-04-16 19:22:24 -0500

    finished bernoulli testing, it works on real data

--- template_speech_rec/bernoulli_em.py
+++ template_speech_rec/bernoulli_em.py
@@ -9,36 +9,56 @@ class Bernoulli_Mixture:
     def __init__(self,num_mix,data_mat):
         self.num_mix = num_mix
         self.height,self.width = data_mat.shape[1:]
+        self.data_mat = data_mat
+        self.num_data = data_mat.shape[0]
         # initializing weights
         self.weights = 1./num_mix * np.ones(num_mix)
         self.init_affinities_templates()
 
 
-    def run_EM(self):
-        likelihood = -np.inf
-        new_likelihood,E_mat = self.compute_likelihoods(data_mat)
-        while new_likelihood - likelihood > tol:
-            likelihood = new_likelihood
+    def run_EM(self,tol):
+        """ EM algorithm
+        First we compute the expected value of the label vector for each point
+        """
+        loglikelihood = -np.inf
+        # First E step plus likelihood computation
+        new_loglikelihood = self.compute_loglikelihoods()
+        while new_loglikelihood - loglikelihood > tol:
+            loglikelihood = new_loglikelihood
+            # M-step
+            self.M_step()
             # E-step
-            bm.set_weights(get_E_weights(E_mat,data_mat))
-            # M_step
-            bm.update_templates(template_weights,data_mat)
-            new_likelihood,E_mat = bm.compute_likelihood(data_mat)
+            new_loglikelihood = self.compute_loglikelihoods()
+ 
+    def M_step(self):
+        self.weights = np.mean(self.affinities,axis=0)
+        for mix_id in xrange(self.num_mix):
+            self.templates[mix_id] = np.sum(self.data_mat * np.tile(self.affinities[:,mix_id],
+                                                                    (self.width,self.height,1)).transpose(),
+                                            axis=0)
+            self.templates[mix_id] /= (self.weights[mix_id] * self.num_data)
+        self.threshold_templates()
+        self.log_templates = np.log(self.templates)
+        self.log_invtemplates = np.log(1-self.templates)
 
+    def threshold_templates(self):
+        self.templates = np.maximum(np.minimum(self.templates,.95),.05)
 
     def init_affinities_templates(self):
         random.seed()
-        num_data = self.data_mat.shape[0]
-        idx = range(num_data)
+        idx = range(self.num_data)
         random.shuffle(idx)
-        self.affinities = np.zeros((num_data,
+        self.affinities = np.zeros((self.num_data,
                                     self.num_mix))
         self.templates = np.zeros((self.num_mix,
                                    self.height,
                                    self.width))
         for mix_id in xrange(self.num_mix):
-            self.affinities[self.num_mix*np.arange(num_data/self.num_mix)+mix_id,mix_id] = 1.
-            self.templates[mix_id] = np.mean(self.data_mat[self.affinities[:,mix_id]==1])
+            self.affinities[self.num_mix*np.arange(self.num_data/self.num_mix)+mix_id,mix_id] = 1.
+            self.templates[mix_id] = np.mean(self.data_mat[self.affinities[:,mix_id]==1],axis=0)
+        self.threshold_templates()
+        self.log_templates = np.log(self.templates)
+        self.log_invtemplates = np.log(1-self.templates)
             
 
     def get_templates(self):
@@ -61,21 +81,30 @@ class Bernoulli_Mixture:
         self.weights = new_weights
         
 
-    def compute_likelihoods(self,data_mat):
-        template_scores = self.get_template_likelihoods(data_mat)
-        return np.dot(template_scores,self.weights), template_scores
+    def compute_loglikelihoods(self):
+        template_logscores = self.get_template_loglikelihoods(self.data_mat)
+        loglikelihoods = template_logscores + np.tile(np.log(self.weights),(self.num_data,1))
+        max_vals = np.amax(loglikelihoods,axis=1)
+        # adjust the marginals by a value to avoid numerical
+        # problems
+        logmarginals_adj = np.sum(np.exp(loglikelihoods - np.tile(max_vals,(self.num_mix,1)).transpose()),axis=1)
+        loglikelihood = np.sum(np.log(logmarginals_adj)) + np.sum(max_vals)
+        self.affinities =loglikelihoods-np.tile(logmarginals_adj+max_vals,
+                                           (self.num_mix,1)).transpose()
+        self.affinities/=np.tile(np.sum(self.affinities,axis=1),(self.num_mix,1)).transpose()
+        return loglikelihood
         
-    def get_template_likelihood(self,datum):
-        return np.prod(self.templates[:,datum > .5],\
-                           axis=1) *\
-                           np.prod((1-self.templates)[:,datum < .5],\
+    def get_template_loglikelihood(self,datum):
+        return np.sum(self.log_templates[:,datum > .5],\
+                           axis=1) +\
+                           np.sum(self.log_invtemplates[:,datum < .5],\
                                        axis=1)
     
-    def get_template_likelihoods(self,data_mat):
+    def get_template_loglikelihoods(self,data_mat):
         """ Assumed to be called whenever
         """
         return np.array(map(lambda x:\
-                                self.get_template_likelihood(x),\
+                                self.get_template_loglikelihood(x),\
                                 data_mat))
 
         
