fbc4a956319ad39556e8d92d5fd486b48a56e1d4
Mark Stoehr <stoehr.mark@gmail.com>
2012-06-13 08:31:34 -0500

    cleaned up parts that were just for testing and experimenting

--- Experiments/061112/extract_local_features.py
+++ Experiments/061112/extract_local_features.py
@@ -1,5 +1,5 @@
 import numpy as np
-root_path = '/home/mark/projects/Template-Speech-Recognition/'
+root_path = '/var/tmp/stoehr/Projects/Template-Speech-Recognition/'
 
 import sys, os, cPickle
 sys.path.append(root_path)
@@ -14,7 +14,7 @@ abst_threshold = np.load(root_path+'Experiments/050812/abst_threshold.npy')
 
 texp = template_exp.\
     Experiment(patterns=[np.array(('aa','r')),np.array(('ah','r'))],
-               data_paths_file=root_path+'Data/WavFilesTrainPaths',
+               data_paths_file=root_path+'Data/WavFilesTrainPaths_feverfew',
                spread_length=3,
                abst_threshold=abst_threshold,
 
@@ -32,8 +32,6 @@ E, edge_feature_row_breaks, edge_orientations =\
 
 import matplotlib.pyplot as plt
 
-plt.imshow(E,interpolation='nearest')
-plt.show()
 
 esp._edge_map_threshold_segments(E,
                                  40,
@@ -43,111 +41,16 @@ esp._edge_map_threshold_segments(E,
                                  edge_feature_row_breaks = edge_feature_row_breaks)
 
 
-plt.imshow(E,interpolation='nearest')
-plt.show()
-
-height, width = E.shape
-patch_height, patch_width = (5,5)
-T = 100
 
 
-# we construct repeating rows that index which column from E.ravel()
-# we should draw the patch from (width-patch_width+1) *
-# (height-patch_height+1), the patches as they vary by the frequency
-# locations have the same column (time) indices so we need to tile
-# them by their times and then repeat those tilings for all the
-# heights
-#
-#
-# tiling by times simply means that we construct the dense row of patches
-# along a fixed contiguous set of frequencies, these look like:
-# array([[[   0,    1,    2,    3,    4],
-       #  [   0,    1,    2,    3,    4],
-       #  [   0,    1,    2,    3,    4],
-       #  [   0,    1,    2,    3,    4],
-       #  [   0,    1,    2,    3,    4]],
+def extract_local_features(E,patch_height,patch_width,lower_quantile,upper_quantile,segment_ms=500,
+                           hop_ms = 5):
+    # segment_ms - number of milliseconds over which we compute the quantile thresholds
+    # hop_ms is says how many milliseconds pass in between each frame
+    segment_length = segment_ms/hop_ms
+    _extract_block_local_features(E,patch_height,patch_width,lower_quantile,upper_quantile)
 
-       # [[   5,    6,    7,    8,    9],
-       #  [   5,    6,    7,    8,    9],
-       #  [   5,    6,    7,    8,    9],
-       #  [   5,    6,    7,    8,    9],
-       #  [   5,    6,    7,    8,    9]],
-
-       # [[  10,   11,   12,   13,   14],
-       #  [  10,   11,   12,   13,   14],
-       #  [  10,   11,   12,   13,   14],
-       #  [  10,   11,   12,   13,   14],
-       #  [  10,   11,   12,   13,   14]],
-
-       # ..., 
-       # [[2305, 2306, 2307, 2308, 2309],
-       #  [2305, 2306, 2307, 2308, 2309],
-       #  [2305, 2306, 2307, 2308, 2309],
-       #  [2305, 2306, 2307, 2308, 2309],
-       #  [2305, 2306, 2307, 2308, 2309]],
-
-       # [[2310, 2311, 2312, 2313, 2314],
-       #  [2310, 2311, 2312, 2313, 2314],
-       #  [2310, 2311, 2312, 2313, 2314],
-       #  [2310, 2311, 2312, 2313, 2314],
-       #  [2310, 2311, 2312, 2313, 2314]],
-
-       # [[2315, 2316, 2317, 2318, 2319],
-       #  [2315, 2316, 2317, 2318, 2319],
-       #  [2315, 2316, 2317, 2318, 2319],
-       #  [2315, 2316, 2317, 2318, 2319],
-       #  [2315, 2316, 2317, 2318, 2319]]])
- #
-#
-#  we then take those above indices and divide them by 5 and add their
-#  remainder when we divide by 5 and this gets us
-#
-col_indices = np.tile(
-    # construct the base set of repeating column indices
-    np.arange(patch_width*(width-patch_width+1)).reshape(width-patch_width+1,1,patch_width,),
-    # repeat the same indices (for columns or times) along each row as those are fixed
-    (1,patch_height,1))
-# change the entries so that way the col[i,:,:] starts with i along the first column
-col_indices = col_indices/patch_width + col_indices%patch_width
-# repeat for each set of frequency bands
-col_indices = np.tile(col_indices,(height-patch_height+1,1,1))
-#
-# construct the row indices
-#
-(np.arange(900,dtype=int)/9).reshape(100,3,3)/10
-# get the base indices
-row_indices = np.tile(np.arange(0,width*patch_height,width),(patch_width,1)).T
-# tile them so that way we have as many as are there are of the col_indices
-row_indices = np.tile(
-    row_indices.reshape(1,patch_height,patch_width),
-    (col_indices.shape[0],1,1)
-    )
-
-
-# now we make the mask that we do our shifting with
-row_add_mask = (np.arange(col_indices.size,dtype=int)/(patch_height*patch_width*(width-patch_width+1))).reshape(col_indices.shape)
-row_add_mask *= width
-
-row_indices += row_add_mask
-
-patches = E.ravel()[row_indices+col_indices]
-#
-#  This test was passed, so our patch computation works
-#  
-# patches_slow = np.zeros(patches.shape)
-# for x in xrange(E.shape[0]-patch_height+1):
-#     for y in xrange(E.shape[1]-patch_width+1):
-#         patches_slow[x*(E.shape[1]-patch_width+1) + y] = E[x:x+patch_height,y:y+patch_height]
-
-
-# assert np.sum (np.abs( patches-patches_slow)) == 0
-
-
-#now we compute the edge frequencies in each patch
-patch_sum_sort = np.argsort(np.sum(np.sum(patches,axis=1),axis=1))
-best_patches = patch_sum_sort[.9*patch_sum_sort.shape[0]:]
-
-def extract_local_features(E,patch_height,patch_width,lower_quantile,upper_quantile):
+def _extract_block_local_features(E,patch_height,patch_width,lower_quantile,upper_quantile):
     height, width = E.shape
     col_indices = np.tile(
         # construct the base set of repeating column indices
