a28ee68fe90f27c797f5290aee0fae7d0582e7f9
Mark Stoehr <stoehr.mark@gmail.com>
2012-07-02 13:24:21 -0500

    adding a new experiment file in to conduct a basic phone classification experiment

---
+++ Experiments/070212/phone_classification.py
@@ -0,0 +1,75 @@
+"""
+Copyright 2012
+Author: Mark Stoehr
+
+Here we perform a phone classification test of the phones
+in timit, we use the Lee Hon list
+
+"""
+
+#root_path = '/var/tmp/stoehr/Projects/Template-Speech-Recognition/'
+root_path = '/home/mark/projects/Template-Speech-Recognition/'
+
+import sys, os, cPickle
+sys.path.append(root_path)
+import numpy as np
+import template_speech_rec.template_experiments as template_exp
+import template_speech_rec.edge_signal_proc as esp
+import template_speech_rec.estimate_template as et
+import template_speech_rec.test_template as tt
+import template_speech_rec.classification as cl
+
+abst_threshold=np.array([.025,.025,.015,.015,
+                                      .02,.02,.02,.02])
+
+# we are going to train a model fo reach phone as given below:
+phns = ['aa', 'ae', 'ah', 'ao', 'aw', 'ax', 'ax-h', 'axr', 'ay', 'b', 'bcl', 'ch', 'd', 'dcl', 'dh', 'dx', 'eh', 'el', 'em', 'en', 'eng', 'epi', 'er', 'ey', 'f', 'g', 'gcl', 'hh', 'hv', 'ih', 'ix', 'iy', 'jh', 'k', 'kcl', 'l', 'm', 'n', 'ng', 'nx', 'ow', 'oy', 'p', 'pau', 'pcl', 'q', 'r', 's', 'sh', 'sp', 't', 'tcl', 'th', 'uh', 'uw', 'ux', 'v', 'w', 'y', 'z', 'zh', 'sil']
+
+texp = template_exp.\
+    Experiment(patterns=[np.array(('aa','r')),np.array(('ah','r'))],
+               #data_paths_file=root_path+'Data/WavFilesTrainPaths_feverfew',
+               data_paths_file=root_path+'Data/WavFilesTrainPaths',
+               spread_length=3,
+               abst_threshold=abst_threshold,
+
+               fft_length=512,num_window_step_samples=80,
+               freq_cutoff=3000,sample_rate=16000,
+               num_window_samples=320,kernel_length=7,
+               offset=3
+               )
+train_data_iter, tune_data_iter =\
+    template_exp.get_exp_iterator(texp,train_percent=.6)
+
+
+train_data_iter.reset_exp()
+phn_set = set()
+while train_data_iter.next():
+    phn_set.update(train_data_iter.phns)
+    
+phn_list = sorted(list(phn_set))
+
+leehon_mapping = { 'aa':'aa',
+                   'ao':'aa',
+                   'ax':'ah',
+                   'ax-h':'ah',
+                   'axr': 'er',
+                   'hv':'hh',
+                   'ix':'ih',
+                   'el':'l',
+                   'em':'m',
+                   'en':'n',
+                   'nx':'n',
+                   'eng':'ng',
+                   'zh':'sh',
+                   'ux':'uw',
+                   'plc':'sil',
+                   'tcl':'sil',
+                   'kcl':'sil',
+                   'bcl':'sil',
+                   'dcl':'sil',
+                   'gcl':'sil',
+                   'h#':'sil',
+                   'pau':'sil',
+                   'epi':'sil',
+                   'sp':'sil'}
+
