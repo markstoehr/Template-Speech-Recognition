696597f2118ebd4f63276b54d3eea0633cef26ee
Mark Stoehr <stoehr.mark@gmail.com>
2012-07-23 00:18:13 -0500

    now we create the test sets for the algorithms to run on

--- Experiments/071912/construct_test_set.py
+++ Experiments/071912/construct_test_set.py
@@ -58,10 +58,11 @@ for phn_id in xrange(1):
     print phn_id, phn_list[phn_id]
     phn_tune_examples = []
     phn_tune_examples_bg = []
+
 for cur_data_pointer in xrange(1,num_test_data+1):
         if cur_data_pointer % 20 == 0:
             print cur_data_pointer
-        E = np.load(data_dir+str(cur_data_pointer)+'tune_E.npy')
+        #E = np.load(data_dir+str(cur_data_pointer)+'tune_E.npy')
         feature_label_transitions = np.load(data_dir+str(cur_data_pointer)+'feature_label_transitions.npy')
         seq_phns = np.load(data_dir+str(cur_data_pointer)+'phns.npy')
         for test_phn_id in xrange(seq_phns.shape[0]):
@@ -81,38 +82,56 @@ np.save('num_target_phns_test',num_target_phns)
 
 stored_bg = np.load('../070212/stored_bg.npy')
 
-for phn_id in xrange(1):
+for phn_id in xrange(phn_list.shape[0]):
     print phn_id, phn_list[phn_id]
     phn_test_examples = np.zeros((num_target_phns[phn_id],
                                   stored_bg.shape[0],
-                                  target_phns_max_length+1),dtype=np.uint8)
-    phn_test_examples_fp = np.memmap(data_dir+phn_list[phn_id]+'_examples.dat',
-                                     dtype=np.uint8,mode='w+',
-                                     shape=(num_target_phns[phn_id],
-                                  stored_bg.shape[0],
-                                  target_phns_max_length+1))
-    phn_test_examples_bg = np.zeros((num_target_phns[phn_id],
-                                     stored_bg.shape[0]),dtype=np.float32)
+                                  target_phns_max_length),dtype=np.uint8)
+    phn_test_bgs = np.zeros((num_target_phns[phn_id],
+                             stored_bg.shape[0]),dtype=np.float64)
+    phn_lengths = np.zeros(num_target_phns[phn_id],dtype=int)
+    cur_example_idx = 0
     for cur_data_pointer in xrange(1,num_test_data+1):
         if cur_data_pointer % 20 == 0:
             print cur_data_pointer
-        E = np.load(data_dir+str(cur_data_pointer)+'tune_E.npy')
+        if phn_id == 0:
+            E = np.load(data_dir+str(cur_data_pointer)+'tune_E.npy')
+            esp._edge_map_threshold_segments(E,
+                                         40,
+                                         1, 
+                                         threshold=.3,
+                                         edge_orientations = test_data_iter.edge_orientations,
+                                         edge_feature_row_breaks = test_data_iter.edge_feature_row_breaks)
+            np.save(data_dir+str(cur_data_pointer)+'thresholded_E.npy',E)
+        else:
+            E = np.load(data_dir+str(cur_data_pointer)+'thresholded_E.npy')
         feature_label_transitions = np.load(data_dir+str(cur_data_pointer)+'feature_label_transitions.npy')
         seq_phns = np.load(data_dir+str(cur_data_pointer)+'phns.npy')
         for test_phn_id in xrange(seq_phns.shape[0]):
             test_phn = seq_phns[test_phn_id]
             # what's the id of the phone in the master list?
             test_phn_id_list = np.arange(phn_list.shape[0])[phn_list == test_phn][0]
-            num_target_phns[test_phn_id_list] +=1
+            if test_phn_id != phn_id:
+                continue
             if test_phn_id + 1 < feature_label_transitions.shape[0]:
-                target_phns_max_length[test_phn_id_list] = max( target_phns_max_length[test_phn_id_list],
-                                                            min(E.shape[1],feature_label_transitions[test_phn_id+1]+offset) - max(0,feature_label_transitions[test_phn_id]-offset))
+                target_phn_end_idx = min(E.shape[1],feature_label_transitions[test_phn_id+1]+offset)
+                target_phn_start_idx = max(0,feature_label_transitions[test_phn_id]-offset)
             else:
-                target_phns_max_length[test_phn_id_list] = max( target_phns_max_length[test_phn_id_list],
-                                                            E.shape[1] - max(0,feature_label_transitions[test_phn_id]-offset))
-    
-
-
-
+                target_phn_end_idx = E.shape[1]
+                target_phn_start_idx = max(0,feature_label_transitions[test_phn_id]-offset)
+            if test_phn_id == 0:
+                bg = stored_bg
+            else:
+                bg = np.minimum(.4,np.maximum(np.mean(E[:,max(0,
+                                                              target_phn_start_idx-20):
+                                                              target_phn_start_idx],axis=1),.1))
+            phn_test_examples[cur_example_idx] = E[:,target_phn_start_idx:target_phn_end_idx]
+            phn_test_bgs[cur_example_idx] = bg.copy()
+            phn_lengths[cur_example_idx] = target_phn_end_idx-target_phn_start_idx
+    np.save(data_dir+phn_list[phn_id]+'_examples.npy',phn_test_examples)
+    np.save(data_dir+phn_list[phn_id]+'_bgs.npy',phn_test_bgs)
+    np.save(data_dir+phn_list[phn_id]+'_lengths.npy',phn_test_lengths)
+            
+            
 
 
