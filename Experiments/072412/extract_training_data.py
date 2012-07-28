6f00b54558c71a2d9215774a68c027e8fa017dce
Mark Stoehr <stoehr.mark@gmail.com>
2012-07-28 16:42:58 -0500

    new script for extracting the example training data

---
+++ Experiments/072412/extract_training_data.py
@@ -0,0 +1,143 @@
+
+import numpy as np
+from collections import defaultdict
+import cPickle
+
+num_data = 4619
+
+root_path = '/var/tmp/stoehr/Projects/Template-Speech-Recognition/'
+data_path = root_path + 'Data/Train/'
+
+import template_speech_rec.template_experiments as template_exp
+# first get the phn lengths and the number of examples
+
+max_phn_lengths = defaultdict(int)
+num_phn_examples = defaultdict(int)
+
+phn_list = np.load(data_path+'phn_list.npy')
+model_classes48_fold_dict = {'ux' : 'uw',
+                             'axr': 'er',
+                             'em': 'm' ,
+                             'nx' : 'n',
+                             'eng': 'ng',
+                             'hv': 'hh',
+                             'pcl': 'cl',
+                             'tcl': 'cl',
+                             'kcl': 'cl',
+                             'qcl': 'cl',
+                             'bcl': 'vcl',
+                             'dcl': 'vcl',
+                             'gcl': 'vcl',
+                             'h#': 'sil',
+                             'pau': 'sil',
+                             '#h': 'sil'}
+model_classes = {}
+for phn in phn_list:
+    if phn not in model_classes48_fold_dict.keys():
+        model_classes[phn] = phn
+    else:
+        model_classes[phn] = model_classes48_fold_dict[phn]
+
+classes_to_phns = defaultdict(list)
+for phn, phn_class in model_classes.items():
+    classes_to_phns[phn_class].append(phn)
+
+
+stored_bg_obj = template_exp.AverageBackground()
+
+    
+for datum in xrange(num_data):
+    phns = np.load(data_path + str(datum+1)+'phns.npy')
+    ftl =  np.load(data_path + str(datum+1)+'feature_label_transitions.npy')
+    E = np.load(data_path + str(datum+1)+'E_spread3.npy')
+    stored_bg_obj(E)
+    for phn_id, phn in enumerate(phns):
+        phn_class = model_classes[phn]
+        max_phn_lengths[phn_class] = max(
+            max_phn_lengths[phn_class],
+            ftl[phn_id+1] - ftl[phn_id])
+        num_phn_examples[phn_class] += 1
+
+np.save(data_path+'averageBackground.npy',stored_bg_obj.E)
+
+class_array = np.array(classes_to_phns.keys(),dtype=int)
+class_to_int = dict( (v,k) for k,v in enumerate(class_array))
+np.save(data_path+'class_array',class_array)
+out = open(data_path + 'class_to_int.pkl','wb')
+cPickle.dump(class_to_int,out)
+out.close()
+
+
+# getting the examples as training examples
+# we use spread 5
+for phn_class in classes_to_phns.keys():
+    class_examples5 = np.empty((num_phn_examples[phn_class],
+                               stored_bg_obj.E.shape[0],
+                               max_phn_lengths[phn_class]),
+                              dtype = np.uint8)
+    class_examples3 = np.empty((num_phn_examples[phn_class],
+                               stored_bg_obj.E.shape[0],
+                               max_phn_lengths[phn_class]),
+                              dtype = np.uint8)
+    example_utt_id_E_loc = np.empty((num_phn_examples[phn_class],
+                                     2),dtype=np.uint32)
+    example_context = np.empty((num_phn_examples[phn_class],
+                                2),dtype=np.uint8)
+    class_examples_bg = np.empty((num_phn_examples[phn_class],
+                                  stored_bg_obj.E.shape[0]),
+                                 dtype=np.float32)
+    class_examples_lengths = np.lengths(num_phn_examples[phn_class],
+                                        dtype = np.uint16)
+    cur_example = 0
+    for datum in xrange(num_data):
+        phns = np.load(data_path + str(datum+1)+'phns.npy')
+        ftl =  np.load(data_path + str(datum+1)+'feature_label_transitions.npy')
+        E3 = np.load(data_path + str(datum+1)+'E_spread3.npy')
+        E5 = np.load(data_path + str(datum+1)+'E_spread5.npy')
+        for phn_id,phn in enumerate(phns):
+            datum_phn_class = model_class[phn]
+            if phn_class != datum_phn_class:
+                continue
+            datum_phn_loc = ftl[phn_id]
+            class_examples5[cur_example] = np.hstack(
+                (E5[:,datum_phn_loc:min(
+                            E5.shape[1],
+                            datum_phn_loc + max_phn_lengths[phn_class])],
+                 np.tile(E[5,-1],(datum_phn_loc+max_phn_lengths[phn_class]
+                                  - E5.shape[1],
+                                  1)).T))
+            class_examples3[cur_example] = np.hstack(
+                (E3[:,datum_phn_loc:min(
+                            E3.shape[1],
+                            datum_phn_loc + max_phn_lengths[phn_class])],
+                 np.tile(E[3,-1],(datum_phn_loc+max_phn_lengths[phn_class]
+                                  - E3.shape[1],
+                                  1)).T))
+            class_examples_lengths[cur_example] = ftl[phn_id+1] - datum_phn_loc
+            if datum_phn_loc > 20:
+                class_examples_bg[cur_example] = np.clip(np.mean(
+                        E3[:,datum_phn_loc-20:
+                                 datum_phn_loc],axis=1),
+                                                         .1,.4)
+            else:
+                class_examples_bg[cur_exampled] = stored_bg_obj.E[:]
+            if phn_id >0:
+                example_context[cur_example][0] = phns[phn_id-1]
+            else:
+                example_context[cur_example][0] = -1
+            if phn_id < phns.shape[0]:
+                example_context[cur_example][1] = phns[phn_id+1]
+            else:
+                example_context[cur_example][1] = -1
+            example_utt_id_E_loc[cur_example][0] = datum
+            example_utt_id_E_loc[cur_example][1] = datum_phn_loc
+            cur_example += 1
+    assert (cur_example == num_phn_examples[phn_class])
+    np.save(data_dir+'class_examples3',class_examples3)
+    np.save(data_dir+'class_examples5',class_examples5)
+    np.save(data_dir+'class_examples_lengths',class_examples_lengths)
+    np.save(data_dir+'class_examples_bgs',class_examples_bg)
+    np.save(data_dir+'example_utt_id_E_loc',example_utt_id_E_loc)
+    np.save(data_dir+'example_context',example_context)
+
+
