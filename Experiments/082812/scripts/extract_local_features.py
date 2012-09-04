82331a83f59b5bb9283b86bcc677b64ec64e526e
markstoehr <stoehr.mark@gmail.com>
2012-09-04 09:43:58 -0500

    script contains completed parts estimation experiment

--- Experiments/082812/scripts/extract_local_features.py
+++ Experiments/082812/scripts/extract_local_features.py
@@ -35,7 +35,7 @@ offset=3
 s_idx =0
 s_fname = s_fnames[s_idx]
 
-bps = np.zeros((0,40,5))
+bps = np.zeros((0,40,5),dtype=np.uint8)
 patch_s_windows = np.zeros((0,1600))
 spec_windows = np.zeros((0,6,6))
 f = open(tmp_data_path+'guide_to_tmp_data.txt','w')
@@ -68,7 +68,7 @@ for s_idx, s_fname in enumerate(s_fnames[:400]):
                                                                        edge_feature_row_breaks,
                                                                        segment_ms=500,
                                                                        hop_ms = 5)
-    bps = np.vstack((bps,bp))
+    bps = np.vstack((bps,bp.astype(np.uint8)))
     s_idx_list.extend(bp.shape[0] *[s_idx])
     # need to capture the fact that we have bigger patches for the spectrogram
     patch_row_indices = elf.generate_patch_row_indices(all_patch_rows,patch_height+1,patch_width+1)-1
@@ -85,35 +85,22 @@ for s_idx, s_fname in enumerate(s_fnames[:400]):
     S_ones = np.zeros(S.shape,dtype=np.uint8)
     S_ones[patch_row_indices,patch_col_indices] = 1
     np.save(tmp_data_path+str(s_idx)+'spec_patch_ones.npy',S_ones)
-    
+
 
 f.close()
 np.save(tmp_data_path+'s_idx_list.npy',np.array(s_idx_list))
 
-np.save(tmp_data_path+'bps.npy',bps)
-bps = np.load(tmp_data_path+'bps.npy')
 
-
-bm = bernoulli_mixture.BernoulliMixture(23,bps[:50000])
-
+bp=np.load(tmp_data_path+'1bp.npy',bp).astype(np.uint8)
+bm = bernoulli_mixture.BernoulliMixture(20,bp)
 bm.run_EM(.000001,min_probability=.001)
-
-bm.run_EM(.000001,min_probability=.001,hard_assignment=True)
-
-
-# do a diagnostic on how many components are in each part
-np.save(tmp_data_path+'bm_templates.npy',bm.templates)
-np.save(tmp_data_path+'bm_affinities.npy',bm.affinities)
+np.save(tmp_data_path+'1bm_templates%d.npy' % num_parts,bm.templates)
+np.save(tmp_data_path+'1bm_affinities%d.npy' % num_parts,bm.affinities)
 bm_templates = bm.templates
 bm_affinities = bm.affinities
-
-
-
 del bm
-
-spec_avg_parts = np.zeros((23,patch_height+1,patch_width+1))
-spec_weight_sums = np.zeros(23)
-
+spec_avg_parts = np.zeros((num_parts,patch_height+1,patch_width+1))
+spec_weight_sums = np.zeros(num_parts)
 s_idx = -1
 for i,affinity in enumerate(bm_affinities):
     if s_idx_list[i] > s_idx:
@@ -121,9 +108,66 @@ for i,affinity in enumerate(bm_affinities):
         S_patches = np.load(tmp_data_path+str(s_idx)+'spec_patch.npy')
         S_patch_start_idx = s_idx
     for part_id, part_affinity in enumerate(affinity):
-        if part_affinity > .05:
+        if part_affinity > .95:
             spec_weight_sums[part_id] += part_affinity
             spec_avg_parts[part_id] += (part_affinity* (S_patches[s_idx-S_patch_start_idx] -  spec_avg_parts[part_id]))/spec_weight_sums[part_id]
+    np.save(tmp_data_path+'1spec_avg_parts%d.npy' % num_parts, spec_avg_parts)
+
+
+
+np.save(tmp_data_path+'bps.npy',bps)
+bps = np.load(tmp_data_path+'bps.npy')
+
+
+
+for num_parts in [10,20,30,50,80,100]:
+    print "Working on number of parts is %d" % num_parts
+    bm = bernoulli_mixture.BernoulliMixture(num_parts,bps[:50000])
+    bm.run_EM(.000001,min_probability=.001)
+    np.save(tmp_data_path+'bm_templates%d.npy' % num_parts,bm.templates)
+    np.save(tmp_data_path+'bm_affinities%d.npy' % num_parts,bm.affinities)
+    bm_templates = bm.templates
+    bm_affinities = bm.affinities
+    del bm
+    spec_avg_parts = np.zeros((num_parts,patch_height+1,patch_width+1))
+    spec_weight_sums = np.zeros(num_parts)
+    s_idx = -1
+    for i,affinity in enumerate(bm_affinities):
+        if s_idx_list[i] > s_idx:
+            s_idx  = s_idx_list[i]
+            S_patches = np.load(tmp_data_path+str(s_idx)+'spec_patch.npy')
+            S_patch_start_idx = s_idx
+        for part_id, part_affinity in enumerate(affinity):
+            if part_affinity > .95:
+                spec_weight_sums[part_id] += part_affinity
+                spec_avg_parts[part_id] += (part_affinity* (S_patches[s_idx-S_patch_start_idx] -  spec_avg_parts[part_id]))/spec_weight_sums[part_id]
+    np.save(tmp_data_path+'spec_avg_parts%d.npy' % num_parts, spec_avg_parts)
+
+
+
+for num_parts in [15,20,25]:
+    print "Working on number of parts is %d" % num_parts
+    bm = bernoulli_mixture.BernoulliMixture(num_parts,bps[:100000])
+    bm.run_EM(.000001,min_probability=.001)
+    np.save(tmp_data_path+'100kbm_templates%d.npy' % num_parts,bm.templates)
+    np.save(tmp_data_path+'100kbm_affinities%d.npy' % num_parts,bm.affinities)
+    bm_templates = bm.templates
+    bm_affinities = bm.affinities
+    del bm
+    spec_avg_parts = np.zeros((num_parts,patch_height+1,patch_width+1))
+    spec_weight_sums = np.zeros(num_parts)
+    s_idx = -1
+    for i,affinity in enumerate(bm_affinities):
+        if s_idx_list[i] > s_idx:
+            s_idx  = s_idx_list[i]
+            S_patches = np.load(tmp_data_path+str(s_idx)+'spec_patch.npy')
+            S_patch_start_idx = s_idx
+        for part_id, part_affinity in enumerate(affinity):
+            if part_affinity > .95:
+                spec_weight_sums[part_id] += part_affinity
+                spec_avg_parts[part_id] += (part_affinity* (S_patches[s_idx-S_patch_start_idx] -  spec_avg_parts[part_id]))/spec_weight_sums[part_id]
+    np.save(tmp_data_path+'100kspec_avg_parts%d.npy' % num_parts, spec_avg_parts)
+    np.save(tmp_data_path+'100kspec_weight_sums%d.npy' % num_parts, spec_weight_sums)
 
 
 print 'hi'
