8b82a107f3fd1fdebb857b195bd596c1037ebf96
Mark Stoehr <stoehr.mark@gmail.com>
2012-10-19 02:16:58 -0500

    added new functions to register templates when the zeroth axis is for time

--- template_speech_rec/estimate_template.py
+++ template_speech_rec/estimate_template.py
@@ -1,4 +1,40 @@
 import numpy as np
+import itertools
+
+def _register_template_time_zero(T,template_time_length):
+    """
+    T is the current example, we work with the time axis
+    being possibly anywhere, we assume that the time
+    axis is along the zeroth dimension
+    """
+    example_time_length = float(T.shape[0])
+    template_time_length = float(template_time_length)
+    return T[np.clip(((np.arange(template_time_length)/template_time_length) * example_time_length + .5).astype(int),0,int(T.shape[0]-1))]
+
+def register_templates_time_zero(examples,lengths,min_prob=.01):
+    mean_length = int(lengths.mean()+.5)
+    registered_examples = np.array(
+        tuple(
+            _register_template_time_zero(example[:length],mean_length)
+            for example,length in itertools.izip(examples,lengths)))
+    return np.clip(registered_examples.mean(0),
+                   min_prob,
+                   1-min_prob), registered_examples
+
+def construct_linear_filter(T,
+                            bgd):
+    """
+    Bgd is the tiled matrix of bgd vectors slaooed onto each other
+    """
+    Bgd = np.tile(bgd,
+                  (T.shape[0],1,1))
+    T_inv = 1. - T
+    Bgd_inv = 1. - Bgd
+    C_exp_inv = T_inv/Bgd_inv
+    c = np.log(C_exp_inv).sum()
+    expW = (T/Bgd) / C_exp_inv
+    return np.log(expW), c
+
 
 def simple_estimate_template(pattern_examples,template_length=None):
     if not template_length:
