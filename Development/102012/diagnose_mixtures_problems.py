9967af61e8a76b598cfde29b426a30ea1ffabe35
Mark Stoehr <stoehr.mark@gmail.com>
2012-10-25 08:07:19 -0500

    created more  printouts to understand the mixtures, was able to understand the experiment

--- Development/102012/diagnose_mixtures_problems.py
+++ Development/102012/diagnose_mixtures_problems.py
@@ -2,8 +2,8 @@
 # to make this basic bit work
 import numpy as np
 
-#root_path = '/home/mark/Template-Speech-Recognition/'
-root_path = '/var/tmp/stoehr/Template-Speech-Recognition/'
+root_path = '/home/mark/Template-Speech-Recognition/'
+#root_path = '/var/tmp/stoehr/Template-Speech-Recognition/'
 data_path = root_path + 'Data/'
 old_exp_path = root_path + 'Notebook/1/'
 old_data_path = old_exp_path + 'data/'
@@ -48,7 +48,9 @@ plt.show()
 # we have confirmed that these are actually what we would hope
 # to see. Going to see how the curves complement each other on an example
 
-aar_templates = np.load(tmp_data_path+'aar_registered_templates_%d_%d.npy' % (num_mix,i))
+aar_templates = ()
+for i in xrange(3):
+    aar_templates += (np.load(tmp_data_path+'aar_registered_templates_%d_%d.npy' % (num_mix,i)),)
 out = open(tmp_data_path+'example_start_end_times_aar_%d_%d.npy' % (num_mix,i),'rb')
 example_start_end_times = cPickle.load(out)
 out.close()
@@ -58,7 +60,7 @@ for i in xrange (num_mix):
     detection_arrays += (np.load(tmp_data_path+'detection_array_aar_new%d_%d.npy' % (num_mix,i)),)
 
 
-clipped_bgd = np.load(old_data_path+'clipped_bgd_102012.npy')
+clipped_bgd = np.load(tmp_data_path+'clipped_bgd_102012.npy')
 
 
 aar_ts = ()
@@ -89,23 +91,28 @@ flts = np.load(data_path + 'Test/'+file_indices[0]+'feature_label_transitions.np
 import matplotlib.pyplot as plt
 
 
-def diagnose_output(utt_i,detection_arrays,colors,markers,flts,example_start_end_times):
+def diagnose_output(i,detection_arrays,colors,markers,example_start_end_times,file_indices):
     plt.close()
     plt.figure()
     min_val = np.inf
     max_val = -np.inf
-    for i in xrange(3):
-        min_val = min(min_val,detection_arrays[i][0].min())
-        max_val = max(max_val,detection_arrays[i][0].max())
-        plt.scatter(np.arange(flts[-1]),detection_arrays[i][0][:flts[-1]],c=colors[i],marker=markers[i])
+    flts = np.load(data_path + 'Test/'+file_indices[i]+'feature_label_transitions.npy')
+    for j in xrange(3):
+        min_val = min(min_val,detection_arrays[j][i].min())
+        max_val = max(max_val,detection_arrays[j][i].max())
+        plt.scatter(np.arange(flts[-1]),detection_arrays[j][i][:flts[-1]],c=colors[j],marker=markers[j])
     x = min_val * np.ones(flts[-1])
-    for s,e in example_start_end_times[i]
+    print flts
+    print example_start_end_times[i]
+    for s,e in example_start_end_times[i]:
+        print s,e
         x[s:e] = max_val
-    plt.scatter(np.arange(flts[-1]),x,c=colors[3],marker=markers[3])
+    plt.scatter(np.arange(flts[-1]),x,c=colors[3],marker='p')
     plt.xlabel('Time Frame')
     plt.ylabel('Detection output')
     plt.show()
 
+diagnose_output(0,detection_arrays,colors,markers,example_start_end_times,file_indices)
 
-x = np.ones(detection_arrays[0].shape[1]) * np.
+#x = np.ones(detection_arrays[0].shape[1]) * np.
 
