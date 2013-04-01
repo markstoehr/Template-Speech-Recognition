38c69479b0777b0f79d479f5f8fa9d42712b7509
markstoehr <stoehr.mark@gmail.com>
2013-03-31 23:09:00 -0500

    fixed argument problem for main function

--- template_speech_rec/get_mistakes.pyx
+++ template_speech_rec/get_mistakes.pyx
@@ -20,13 +20,14 @@ ctypedef np.int16_t INT_t
 def get_example_scores_metadata(np.ndarray[ndim=2,dtype=BOOL_t] example_mask,
                  np.ndarray[ndim=2,dtype=UINT_t] classify_locs,
                  np.ndarray[ndim=2,dtype=DTYPE_t] classify_array,
+                 np.ndarray[ndim=1,dtype=UINT_t] classify_lengths,
                  int num_examples):
     cdef int num_utts = example_mask.shape[0]
     cdef int max_utt_len = example_mask.shape[1]
     cdef int utt_id,phn_id,cur_example_id
     cdef np.ndarray[ndim=1,dtype=DTYPE_t] example_scores = np.zeros(num_examples,
                                                                     dtype=DTYPE)
-    
+
     cdef np.ndarray[ndim=2,dtype=UINT_t] example_metadata = np.zeros(
         (num_examples,
          3),
