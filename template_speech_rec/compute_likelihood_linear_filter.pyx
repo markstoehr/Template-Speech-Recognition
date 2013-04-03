71f8e758c6057993f2db69f7d4a2c4f1200376b0
markstoehr <stoehr.mark@gmail.com>
2013-04-02 22:09:27 -0500

    can do detection on 2-d features

--- template_speech_rec/compute_likelihood_linear_filter.pyx
+++ template_speech_rec/compute_likelihood_linear_filter.pyx
@@ -101,6 +101,45 @@ def detect_float(np.ndarray[ndim=3,
                             part_identity])
     return detect_scores
 
+def detect_float2d(np.ndarray[ndim=2,
+                      dtype=DTYPE_t] F,
+           np.ndarray[ndim=2,
+                      dtype=DTYPE_t] LF):
+    """
+    Parameters:
+    ===========
+    F: np.ndarray[ndim=3,dtype=UINT_t]
+        Features for an example utterance that we have fit with the
+        intermediate features plus downsampled. Dimensions
+        of F and LF are assumed to be (time,frequency,patch_type)
+    LF: np.ndarray[ndim=3,dtype=DTYPE_t]
+        Linear filter for computing the likelihood
+    Output:
+    =======
+    detect_scores: np.ndarray[ndim=1,dtype=DTYPE_t]
+        Performs spreading 
+    """
+    cdef np.uint16_t F_dim_0 = F.shape[0]
+    cdef np.uint16_t F_dim_1 = F.shape[1]
+    cdef np.uint16_t LF_dim_0 = LF.shape[0]
+    cdef np.uint16_t num_detections = F_dim_0 - LF_dim_0 +1
+    cdef np.ndarray[ndim=1,dtype=DTYPE_t] detect_scores = np.zeros(F_dim_0,
+                                                                   dtype=DTYPE)
+    cdef np.uint32_t cur_detection,filter_timepoint,frequency,part_identity,lo_spread_0_idx,hi_spread_0_idx,lo_spread_1_idx,hi_spread_1_idx,x0,x1
+    # cur_detection is the time point in the overall vector
+    for cur_detection in range(num_detections):
+        # filter_timepoint is where we are in the filter for
+        # computing these parallel convolutions
+        for filter_timepoint in range(LF_dim_0):
+            for frequency in range(F_dim_1):
+                detect_scores[cur_detection] += (
+                        
+                        LF[filter_timepoint,
+                           frequency] 
+                        * F[cur_detection+filter_timepoint,
+                            frequency])
+    return detect_scores
+
 
 
 def detect_spectral(np.ndarray[ndim=2,
