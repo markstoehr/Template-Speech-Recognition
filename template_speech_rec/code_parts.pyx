e710a5b7e6136c6ad2081aa26baed75e5265f5c3
Mark Stoehr <stoehr.mark@gmail.com>
2013-04-03 23:38:36 -0500

    developing fast coding methods for the augmented parts

--- template_speech_rec/code_parts.pyx
+++ template_speech_rec/code_parts.pyx
@@ -135,6 +135,82 @@ def code_parts(np.ndarray[ndim=3,dtype=UINT_t] X,
     return out_map
 
 
+def code_parts_mag_aux(np.ndarray[ndim=3,dtype=UINT_t] X_edges,
+                       np.ndarray[ndim=2,dtype=UINT_t] X_mags,
+                       np.ndarray[ndim=2,dtype=UINT_t] X_aux,
+                       np.ndarray[ndim=4,dtype=DTYPE_t] log_parts,
+                       np.ndarray[ndim=4,dtype=DTYPE_t] log_invparts,
+                       np.ndarray[ndim=2,dtype=DTYPE_t] log_mag_parts,
+                       np.ndarray[ndim=2,dtype=DTYPE_t] log_mag_invparts,
+                       np.ndarray[ndim=2,dtype=DTYPE_t] log_aux_parts,
+                       np.ndarray[ndim=2,dtype=DTYPE_t] log_aux_invparts,
+                       np.ndarray[ndim=2,dtype=UINT_t] X_thresh_mask):
+    """
+    At each location of `X`, find the log probabilities for each part and location. Outputs these part assignments in the same data dimensions as `X`. We also have auxiliary data containing information about the frequency channel and the magnitude of features in the frequency channel as well as wiener entropy, zero crossing rate, and the log power.  These are incorporated into the matching processing. Neighborhoods of `X` with edge counts lower than `threshold` are regarded as background and assigned zero.
+
+    Parameters
+    ----------
+    X : ndarray[ndim=3,dtype=np.uint8]
+        The first two dimensions of the array specify locations. The last one specifies a binary edge type. The value ``X[s,t,e]`` is 1 iff there is an edge of type `e` detected at location `(s,t)`.
+    log_parts : ndarray[ndim=4]
+        We have a Bernoulli mixture model defined over patches of the input image. The `log_parts` is a logarithm applied to the array of edge probability maps for each part. Array of shape `(K, S, T, E)`, where `K` is the number of mixture component, `S` and `T` the shape of the data, and `E` the number of edges. The value of ``log_parts[k,s,t,e]`` is the log probability of observing an edge `e` at location `(s,t)`, conditioned on the mixture component being `k`.
+    log_invparts : ndarray[ndim=4]
+        Preprocessed inverse of `log_parts`, i.e. ``log(1-exp(log_parts))``.
+    X_thresh_mask : ndarray[ndim=2,dtype=np.uint8]
+        Has the same first two dimensinos as X, indicates which entries of
+        X are going to be analyzed for patch fitting
+    
+    Returns
+    -------
+    out_map : ndarray[ndim=3] 
+        Array of shape `(S, T, K+1)`. There are two cases, either the third dimension is `(0, -inf, -inf, ...)`, when there are insufficient edges in the neighborhood of a location. Otherwise, `out_map[s,t,i+1]` is the log likelihood of part `i` at location `(s,t)`. Additionally, `out_map[s,t,0]` is equal to `-inf`.
+    """
+
+    cdef unsigned int num_parts = log_parts.shape[0]
+    cdef unsigned int part_x_dim = log_parts.shape[1]
+    cdef unsigned int part_y_dim = log_parts.shape[2]
+    cdef unsigned int part_z_dim = log_parts.shape[3]
+    cdef unsigned int X_x_dim = X_edges.shape[0]
+    cdef unsigned int X_y_dim = X_edges.shape[1]
+    cdef unsigned int X_z_dim = X_edges.shape[2]
+    cdef unsigned int new_x_dim = X_x_dim - part_x_dim + 1
+    cdef unsigned int new_y_dim = X_y_dim - part_y_dim + 1
+    cdef unsigned int i_start,j_start,i_end,j_end,count,i,j,z,k
+    # we have num_parts + 1 because we are also including some regions as being
+    # thresholded due to there not being enough edges
+    
+
+    cdef np.ndarray[dtype=DTYPE_t, ndim=3] out_map = np.zeros((new_x_dim,
+                                                                        new_y_dim,
+                                                                        num_parts+1),dtype=DTYPE)
+    # The first cell along the num_parts+1 axis contains a value that is either 0
+    # if the area is deemed to have too few edges or min_val if there are sufficiently many
+    # edges, min_val is just meant to be less than the value of the other cells
+    # so when we pick the most likely part it won't be chosen
+
+    for i_start in range(X_x_dim-part_x_dim+1):
+        i_end = i_start + part_x_dim
+        for j_start in range(X_y_dim-part_y_dim+1):
+            j_end = j_start + part_y_dim
+            count = _count_edges(X,i_start,i_end,j_start,j_end,X_z_dim)
+            if count >= threshold:
+                out_map[i_start,j_start,0] = -np.inf
+                for i in range(part_x_dim):
+                    for j in range(part_y_dim):
+                        for z in range(X_z_dim):
+                            if X[i_start+i,j_start+j,z]:
+                                for k in range(num_parts):
+                                    out_map[i_start,j_start,k+1] += log_parts[k,i,j,z]
+                            else:
+                                for k in range(num_parts):
+                                    out_map[i_start,j_start,k+1] += log_invparts[k,i,j,z]
+            else:
+                out_map[i_start,j_start,0] = 0.0
+                
+    return out_map
+
+
+
 
 def code_parts_fast(np.ndarray[ndim=3,dtype=UINT_t] X,
                     np.ndarray[ndim=4,dtype=DTYPE_t] log_parts,
