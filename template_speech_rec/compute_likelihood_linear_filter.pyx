1e5ce3c7a002392acd431d11894a74afe259d31a
Mark Stoehr <stoehr.mark@gmail.com>
2013-06-18 08:22:57 -0500

    handles floating point data

--- template_speech_rec/compute_likelihood_linear_filter.pyx
+++ template_speech_rec/compute_likelihood_linear_filter.pyx
@@ -101,6 +101,66 @@ def detect_float(np.ndarray[ndim=3,
                             part_identity])
     return detect_scores
 
+def detect_float_max(np.ndarray[ndim=3,
+                      dtype=DTYPE_t] F,
+           np.ndarray[ndim=4,
+                      dtype=DTYPE_t] LF,
+                     np.ndarray[ndim=1,dtype=DTYPE_t] cs):
+    """
+    Parameters:
+    ===========
+    F: np.ndarray[ndim=3,dtype=UINT_t]
+        Features for an example utterance that we have fit with the
+        intermediate features plus downsampled. Dimensions
+        of F and LF are assumed to be (time,frequency,patch_type)
+    LF: np.ndarray[ndim=3,dtype=DTYPE_t]
+        Linear filter for computing the likelihood
+    Output:
+    =======
+    detect_scores: np.ndarray[ndim=1,dtype=DTYPE_t]
+        Performs spreading 
+    """
+    cdef np.uint16_t F_dim_0 = F.shape[0]
+    cdef np.uint16_t F_dim_1 = F.shape[1]
+    cdef np.uint16_t F_dim_2 = F.shape[2]
+    cdef np.uint16_t LF_dim_1 = LF.shape[1]
+    cdef np.uint16_t num_LFs = LF.shape[0]
+    cdef np.uint16_t num_detections = F_dim_0 - LF_dim_1 +1
+    cdef np.ndarray[ndim=1,dtype=DTYPE_t] detect_scores = np.zeros(F_dim_0,
+                                                                   dtype=DTYPE)
+    cdef np.ndarray[ndim=1,dtype=np.uint16_t] max_detector = np.zeros(F_dim_0,
+                                                                   dtype=np.uint16)
+    cdef DTYPE_t cur_detection_val
+    cdef np.uint16_t cur_detector
+    cdef np.uint32_t cur_detection,filter_timepoint,frequency,part_identity,lo_spread_0_idx,hi_spread_0_idx,lo_spread_1_idx,hi_spread_1_idx,x0,x1
+    # cur_detection is the time point in the overall vector
+    for cur_detection in range(num_detections):
+        # filter_timepoint is where we are in the filter for
+        # computing these parallel convolutions
+        for cur_detector in range(num_LFs):
+            cur_detection_val = cs[cur_detector]
+            for filter_timepoint in range(LF_dim_1):
+                for frequency in range(F_dim_1):
+                    for part_identity in range(F_dim_2):
+                        cur_detection_val += (
+                        
+                            LF[cur_detector,filter_timepoint,
+                               frequency,
+                               part_identity] 
+                            * F[cur_detection+filter_timepoint,
+                            frequency,
+                            part_identity])
+
+            if cur_detector == 0:
+                detect_scores[cur_detection] = cur_detection_val
+                max_detector[cur_detection] = cur_detector
+            else:
+                if cur_detection_val > detect_scores[cur_detection]:
+                    detect_scores[cur_detection] = cur_detection_val
+                    max_detector[cur_detection] = cur_detector
+    return detect_scores, max_detector
+
+
 def detect_float2d(np.ndarray[ndim=2,
                       dtype=DTYPE_t] F,
            np.ndarray[ndim=2,
