dd2c39b5df6472aee4599c6387347a8bd253b69b
markstoehr <stoehr.mark@gmail.com>
2013-06-18 15:10:35 -0500

    wrote more tests to begin comparison between this EM and the standard bernoulli EM in use

--- Notebook/48/test_EM_loc_length.py
+++ Notebook/48/test_EM_loc_length.py
@@ -25,8 +25,8 @@ def createRandomModels(min_lengths,
                                                                        num_ridges,
                                                                        blurring_bandwidth,
                                                                        min_probability=min_probability)
-    
-    
+
+
     return np.clip(models,min_probability,1-min_probability)
 
 
@@ -34,7 +34,7 @@ def _createRandomModel( min_length,
                       num_features,
                       num_lengths,
                       num_ridges,
-                      
+
                       blurring_bandwidth,
                       min_probability=0.01):
     """
@@ -72,33 +72,33 @@ def _createRandomModel( min_length,
                                     np.arange(
             max_model_length)
                                     )
-    
+
     num_approx_terms = 1000
 
     for ridge_id in xrange(num_ridges):
         # get random start and end points
         ridge_start_y = np.random.randint(max_model_length)
         ridge_start_x = np.random.randint(num_features)
-        
+
         ridge_end_y = np.random.randint(max_model_length)
         ridge_end_x = np.random.randint(num_features)
-        
-        
+
+
         mask  = np.abs( model_yy - (ridge_end_y - ridge_start_y)* (model_xx - ridge_start_x)/(ridge_end_x - ridge_start_x) - ridge_start_y) <= .01
 
-# += ((( model_xx - ridge_start_x)/(ridge_end_x - ridge_start_x) >= i*1./num_approx_terms) 
-#                     * (( model_xx - ridge_start_x)/(ridge_end_x - ridge_start_x) <= (i+1)*1./num_approx_terms) 
-#                     * (( model_yy - ridge_start_y)/(ridge_end_y - ridge_start_y) <= (i+1)*1./num_approx_terms) 
+# += ((( model_xx - ridge_start_x)/(ridge_end_x - ridge_start_x) >= i*1./num_approx_terms)
+#                     * (( model_xx - ridge_start_x)/(ridge_end_x - ridge_start_x) <= (i+1)*1./num_approx_terms)
+#                     * (( model_yy - ridge_start_y)/(ridge_end_y - ridge_start_y) <= (i+1)*1./num_approx_terms)
 #                     * (( model_yy - ridge_start_y)/(ridge_end_y - ridge_start_y) >= i*1./num_approx_terms) )
         try:
             model[mask] = 1.
         except:
             import pdb; pdb.set_trace()
-        
-    
+
+
     return np.clip(gaussian_filter(model,blurring_bandwidth),min_probability,1-min_probability)
-    
-        
+
+
 def createRandomData(models,
                      num_examples,
                      min_lengths,
@@ -126,20 +126,20 @@ def createRandomData(models,
 
             examples[i,:start_time] = (np.random.rand(start_time,
                                                       num_features) <= background_model).astype(np.uint8)
-        
+
         examples[i,start_time:start_time+min_lengths[model_id]+add_length] = (
             np.random.rand(min_lengths[model_id]+add_length,
                             num_features) <= models[model_id,:min_lengths[model_id]+add_length]).astype(np.uint8)
-        
+
         model_end_time = start_time+min_lengths[model_id] + add_length
         if model_end_time < max_example_length:
             examples[i,model_end_time:] = (
                 np.random.rand(max_example_length-model_end_time,
                                 num_features) <= background_model).astype(np.uint8)
-    
+
     return examples,example_start_times
-                         
-    
+
+
 def testSimpleComputeLogLikelihood():
     np.random.seed(0)
     min_lengths = np.array((20,))
@@ -172,37 +172,118 @@ def testSimpleComputeLogLikelihood():
                          num_start_times=num_start_times,
                          background_model=background_model,
                          )
-    
+
     L = np.zeros((num_examples,num_models,num_start_times,
                   num_lengths))
 
     max_loglikes_by_example = np.zeros(num_examples)
 
-    
+
 
     (E_bgd_frame_scores,
      E_bgd_scores_prefix,
-     E_all_bgd_scores) = computeBackgroundLogLikelihoods(E,np.log(background_model),np.log(1-background_model))
+     E_all_bgd_scores) = EM_loc_length.computeBackgroundLogLikelihoods(E,np.log(background_model),np.log(1-background_model))
 
     log_weights = np.log(np.ones((num_models,num_lengths)) * 1./(num_models * num_lengths))
 
-    computeLogLikelihood(E,L,
+    EM_loc_length.computeLogLikelihood(E,L,
                          min_lengths,
                          example_labeled_start_times,
                          log_weights,
                          log_M,
                          log_inv_M,
-                         max_loglikes_by_example, 
+                         max_loglikes_by_example,
                          E_bgd_frame_scores,
                        E_bgd_scores_prefix,E_all_bgd_scores)
 
     example_lls = np.zeros((num_examples,num_lengths))
     L[:] = 0.
-    out_ll = EStep(E,L,min_lengths,
+    out_ll = EM_loc_length.EStep(E,L,min_lengths,
                    example_labeled_start_times,log_weights,log_M,
                    log_inv_M,max_loglikes_by_example, E_bgd_frame_scores,
                    E_bgd_scores_prefix,E_all_bgd_scores,example_lls)
 
+    weights = np.zeros(log_weights.shape)
+    M_normalizations = np.zeros(log_M.shape)
+    M[:] = 0
+
+    EM_loc_length.MStep(E,L,min_lengths,
+           example_labeled_start_times,M,M_normalizations,weights,
+          example_lls
+           )
+
+def testSimpleTwoModelComputeLogLikelihood():
+    np.random.seed(0)
+    min_lengths = np.array((20,20))
+    num_features=100
+    num_lengths=1
+    num_ridges=4
+    min_probability=0.01
+    M = createRandomModels(min_lengths=min_lengths,
+                           num_features=num_features,
+                           num_lengths=num_lengths,
+                           num_ridges=num_ridges,
+                           blurring_bandwidth=2,
+                           min_probability=min_probability
+                           )
+    log_M = np.log(M)
+    log_inv_M = np.log(1-M)
+
+    num_models = len(M)
+
+    background_model = M.min() * np.ones(num_features)
+    # generate the data set
+    # num_start_times specifies how many start times are allowed
+    # for the models
+    num_start_times = 1
+    num_examples=50
+    E,example_labeled_start_times = createRandomData(models=M,
+                         num_examples=num_examples,
+                         min_lengths=min_lengths,
+                         num_lengths=num_lengths,
+                         num_start_times=num_start_times,
+                         background_model=background_model,
+                         )
+
+    L = np.zeros((num_examples,num_models,num_start_times,
+                  num_lengths))
+
+    max_loglikes_by_example = np.zeros(num_examples)
+
+
+
+    (E_bgd_frame_scores,
+     E_bgd_scores_prefix,
+     E_all_bgd_scores) = EM_loc_length.computeBackgroundLogLikelihoods(E,np.log(background_model),np.log(1-background_model))
+
+    log_weights = np.log(np.ones((num_models,num_lengths)) * 1./(num_models * num_lengths))
+
+    EM_loc_length.computeLogLikelihood(E,L,
+                         min_lengths,
+                         example_labeled_start_times,
+                         log_weights,
+                         log_M,
+                         log_inv_M,
+                         max_loglikes_by_example,
+                         E_bgd_frame_scores,
+                       E_bgd_scores_prefix,E_all_bgd_scores)
+
+    example_lls = np.zeros((num_examples,num_lengths))
+    L[:] = 0.
+    out_ll = EM_loc_length.EStep(E,L,min_lengths,
+                   example_labeled_start_times,log_weights,log_M,
+                   log_inv_M,max_loglikes_by_example, E_bgd_frame_scores,
+                   E_bgd_scores_prefix,E_all_bgd_scores,example_lls)
+
+    weights = np.zeros(log_weights.shape)
+    M_normalizations = np.zeros(log_M.shape)
+    M_old = M.copy()
+    M[:] = 0
+
+    EM_loc_length.MStep(E,L,min_lengths,
+           example_labeled_start_times,M,M_normalizations,weights,
+          example_lls
+           )
     
 
 def testComputeLogLikelihood():
@@ -238,13 +319,13 @@ def testComputeLogLikelihood():
                          num_start_times=num_start_times,
                          background_model=background_model,
                          )
-    
+
     L = np.zeros((num_examples,num_models,num_start_times,
                   num_lengths))
 
     max_loglikes_by_example = np.zeros(num_examples)
 
-    
+
 
     (E_bgd_frame_scores,
      E_bgd_scores_prefix,
@@ -258,13 +339,16 @@ def testComputeLogLikelihood():
                          log_weights,
                          log_M,
                          log_inv_M,
-                         max_loglikes_by_example, 
+                         max_loglikes_by_example,
                          E_bgd_frame_scores,
                        E_bgd_scores_prefix,E_all_bgd_scores)
-    
+
     EStep(E,L,min_lengths,
            example_labeled_start_times,log_M,
            log_inv_M,max_loglikes_by_example, mean_log_like_bgd_model,
            E_bgd_scores_prefix,E_all_bgd_scores)
-    
-    
+
+
+
+def testMStep():
+    pass
